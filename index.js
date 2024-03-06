b1fd9b429568a65c64121324cf7978a628d15f9b
Isabela <115189675+Istnss@users.noreply.github.com>
2024-03-06 19:02:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {});
 
 function onChangeBg() {
     const container = document.querySelector('.container-first');
